23aaa084a7a5916f903d408725a00a98a90b0944
Evgeniy Karasev <karasevevg@gmail.com>
2021-03-14 14:54:01 +0300

    Using jpa methods and classes instead of hibernate

--- src/main/java/com/cronnoss/springboot/hr_staff_rest_api/dao/DepartmentDAOImpl.java
+++ src/main/java/com/cronnoss/springboot/hr_staff_rest_api/dao/DepartmentDAOImpl.java
@@ -1,12 +1,13 @@
 package com.cronnoss.springboot.hr_staff_rest_api.dao;
 
 import com.cronnoss.springboot.hr_staff_rest_api.entity.Department;
-import org.hibernate.Session;
-import org.hibernate.query.Query;
+//import org.hibernate.Session;
+//import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
+import javax.persistence.Query;
 import java.util.List;
 
 @Repository
@@ -18,32 +19,43 @@ public class DepartmentDAOImpl implements DepartmentDao {
     @Override
     public List<Department> getAllDepartments() {
 
-        Session session = entityManager.unwrap(Session.class);
+//        Session session = entityManager.unwrap(Session.class);
+//        Query<Department> query = session.createQuery("from Department"
+//                , Department.class);
+//        List<Department> allDepartments = query.getResultList();
 
-        Query<Department> query = session.createQuery("from Department"
-                , Department.class);
+        Query query = entityManager.createQuery("from Department", Department.class);
         List<Department> allDepartments = query.getResultList();
-
         return allDepartments;
     }
 
     @Override
     public void saveDepartment(Department department) {
-        Session session = entityManager.unwrap(Session.class);
-        session.saveOrUpdate(department);
+//        Session session = entityManager.unwrap(Session.class);
+//        session.saveOrUpdate(department);
+
+        Department newDepartment = entityManager.merge(department);
+        department.setDepid(newDepartment.getDepid());
     }
 
     @Override
     public Department getDepartment(int depid) {
-        Session session = entityManager.unwrap(Session.class);
-        Department department = session.get(Department.class, depid);
+//        Session session = entityManager.unwrap(Session.class);
+//        Department department = session.get(Department.class, depid);
+
+        Department department = entityManager.find(Department.class, depid);
         return department;
     }
 
     @Override
     public void deleteDepartment(int depid) {
-        Session session = entityManager.unwrap(Session.class);
-        Query<Department> query = session.createQuery("delete from Department " +
+//        Session session = entityManager.unwrap(Session.class);
+//        Query<Department> query = session.createQuery("delete from Department " +
+//                "where depid =:depid");
+//        query.setParameter("depid", depid);
+//        query.executeUpdate();
+
+        Query query = entityManager.createQuery("delete from Department " +
                 "where depid =:depid");
         query.setParameter("depid", depid);
         query.executeUpdate();
